0fc3463cebae0c9c0bf8f7538681625ef9b08dfb
Alexander Yavtushenko <yavtushenko.fe@gmail.com>
2026-05-30 18:11:00 +0300

    * middlewares :(response, validation)

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,5 @@
 const responseMiddleware = (req, res, next) => {
-  // TODO: Implement middleware that returns result of the query
+  res.status(200).json(res.data)
   next();
 };
 
--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,12 +1,54 @@
 import { USER } from "../models/user.js";
 
 const createUserValid = (req, res, next) => {
-  // TODO: Implement validatior for USER entity during creation
+  const body = req.body; 
+
+  if ("id" in body) {
+    return res.status(400).json({
+      error: true,
+      message: "id is not allowed",
+    })
+  }
+
+  const requiredFields = Object.keys(USER).filter(key => key !== "id");
+
+  const missingFields = requiredFields.filter(field => body[field] === undefined);
+
+  if (missingFields.length) {
+    return res.status(400).json({
+      error: true,
+      message: `Missing fields: ${missingFields.join(", ")}`
+    })
+  }
+
   next();
 };
 
 const updateUserValid = (req, res, next) => {
-  // TODO: Implement validatior for user entity during update
+  const body = req.body;
+
+  if ("id" in body) {
+    return res.status(400).json({
+      error: true,
+      message: "id is not allowed",
+    })
+  }
+
+  const allowedFields = Object.keys(USER).filter(key => key !== "id");
+
+  const hasAtLeastOneField = allowedFields.some(field => field in body);
+
+  const hasOnlyAllowedFields = Object.keys(body).every(key => key => allowedFields.includes(key))
+
+
+
+  if (!hasAtLeastOneField || !hasOnlyAllowedFields) {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid update body"
+    })
+  } 
+
   next();
 };
 
