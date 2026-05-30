16cfaa37961f3e99bd093a8e7f607934ef31f961
Alexander Yavtushenko <yavtushenko.fe@gmail.com>
2026-05-30 19:11:45 +0300

    + UserService CRUD logic

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,73 @@
 import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
-  // TODO: Implement methods to work with user
+  getAll() {
+    return userRepository.getAll()
+  }
+
+  getOne(id) {
+    const user = userRepository.getOne({ id });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return user
+  }
+
+  create(data) {
+    const existingByEmail = userRepository.getOne({ email: data.email });
+
+    if (existingByEmail) {
+      throw new Error("Email already exists");
+    }
+
+    const existingByPhone = userRepository.getOne({ phone: data.phone });
+
+    if (existingByPhone) {
+      throw new Error("Phone already exists");
+    }
+
+    return userRepository.create(data)
+  }
+
+  update(id, data) {
+    const user = userRepository.getOne({ id });
+
+    if (!user) {
+      throw new Error("User not found");    
+    }
+
+    if (data.email) {
+      const existingByEmail = userRepository.getOne({ email: data.email });
+
+      if (existingByEmail && existingByEmail.id !== id) {
+        throw new Error("Email already exists");
+      }
+    }
+
+    if (data.phone) {
+      const existingByPhone = userRepository.getOne({
+        phone: data.phone,
+      });
+
+      if (existingByPhone && existingByPhone.id !== id) {
+        throw new Error("Phone already exists");
+      }
+    }
+
+  }
+
+  delete(id) {
+    const user = userRepository.getOne({ id });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    userRepository.delete(id);
+    
+    return user;
+  }
 
   search(search) {
     const item = userRepository.getOne(search);
